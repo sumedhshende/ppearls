6de95873ab8abc7f405903f6d9df4106931aea38
Sumedh Shende <sumedh.shende@hotmail.com>
2018-11-07 19:28:53 -0800

    Added test cases

--- rot.cpp
+++ rot.cpp
@@ -4,43 +4,55 @@
 #include<vector>
 #include<ctime>
 
-using namspace std;
+using namespace std;
 
 void reverse(vector<int>& vArr, int s, int e) {
 	int f = s;
 	int l = e;
-	for(int f=0; f<l; f++, --l) {
+	for(; f<l; f++, --l) {
 		int t = vArr[f];	
 		vArr[f] = vArr[l];
 		vArr[l] = t;
 	}
 }
-void rotate(vector<int>& vArr, int point) {
-	reverse(vArr, 0, point);
-	reverse(vArr, point+1, vArr.size());
-	reverse(vArr, 0, vArr.size());
-}
 void printv(vector<int>& v) {
 	for(auto& i : v) {
-		cout << *i << " ";
+		cout << i << " ";
 	}
 	cout << endl;
 }
+void rotate(vector<int>& vArr, int point) {
+	if (point  < 1) return ;
+	reverse(vArr, 0, point-1);
+	printv(vArr);
+	reverse(vArr, point, vArr.size()-1);
+	printv(vArr);
+	reverse(vArr, 0, vArr.size()-1);
+}
+
 void test_rotate() {
 	srand(time(NULL));
 	for(int i=0; i<10; i++) {
+		int size = rand() % 5 + 1;
 		vector<int> v;
-		int size = rand() % 20 + 1;
+		vector<int> r(size);	
 		for(int j=0; j<size; j++) {
-			v.push_back(rand() % 50 + 1);
-			
+			int e = rand() % 20 + 1;
+			v.push_back(e);
+			r[size-j-1] = e;
 		}
-		cout << "Original Array-------------------------------------------" << endl;
-		printv(v);
 		int rotpoint = rand() % size ;
+		cout << "Original Array-------------- " << rotpoint << "  -------" << endl;
+		printv(v);
 		rotate(v, rotpoint);	
 		cout << "Rotated Array-------------------------------------------" << endl;		
 		printv(v);
+		printv(r);
+		for(int k=0; k<size; k++) {
+			if(v[k] != r[k]) cout << " --------------Test Failed--------------------" << endl;
+		}
+		cout << " --------------Test Failed--------------------" << endl;
+	}
 }		
 
 int main() {
